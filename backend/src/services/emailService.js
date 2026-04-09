444784299e9edf4b1dcfd2d41b0561cf05670c51
Vijay Katake <vijukatake@gmail.com>
2026-04-09 22:56:18 +0530

    Partner Registeration Successful. Email Error.

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -10,6 +10,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000, // 10 sec
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 // ─── Generic Send Email ───────────────────────────────────
