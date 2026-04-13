012de1bdac49fa28a084d0b08e5f82fdecc8fef6
Vijay Katake <vijukatake@gmail.com>
2026-04-13 18:06:47 +0530

    DB Issue Solution

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
-
+console.log("DB CONFIG:", {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
